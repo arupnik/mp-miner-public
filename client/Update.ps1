2489968b642912a14a4bed88b2f17c71ee7d402b
arupnik <andrej.r@gmail.com>
2018-07-19 07:27:50 +0200

    [Release]: 0.0.47 ==> 0.1.0

--- client/Update.ps1
+++ client/Update.ps1
@@ -1,6 +1,6 @@
 ﻿# ==================================================================
 # mpMiner updater
-# Current client version: 0.0.47
+# Current client version: 0.1.0
 # ==================================================================
 function Unzip($zipfile, $outdir) {
 
