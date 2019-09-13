65c097364d51c445043874b7650d7c00d47e3091
Andrej <andrej.r@gmail.com>
2019-09-13 12:41:00 +0200

    [Release-WinApp] 0.1.44 --> 0.1.45

--- client/Update.ps1
+++ client/Update.ps1
@@ -1,6 +1,6 @@
 # ==================================================================
 # mpMiner updater
-# Current client version: 0.1.44
+# Current client version: 0.1.45
 # ==================================================================
 function Unzip($zipfile, $outdir) {
 
